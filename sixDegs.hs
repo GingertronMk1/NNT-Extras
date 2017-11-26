af61bc55c20eb78f612dc9b2db734ade38ddfd52
Jack Ellis <jackellis1504@gmail.com>
2017-11-26 12:50:01 +0000

    Lots of stuff again
    - Shortened excludedShows to basically use what was ffGen inline now
    
    - Every instance of 'let x in y' has been changed st the 'let' part is on
    one line and the 'in' part is on the next
    
    - Added "\n" as a character variable
    
    - Readded headAndLast to ppAdjCheck as hl
    
    - All instances of isInfixOf have now been modified to use isPrefixOf
    appropriately
    
    - Reflattened long pointfree expressions
    
    - Finally and most usefully: stripShit now uses elem rather than lots of
    'or's

--- sixDegs.hs
+++ sixDegs.hs
@@ -1,6 +1,6 @@
 -------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- We're first going to import some things:
--- - Data.List for isInfixOf, sort, and group (isInfixOf is used so much in this)
+-- - Data.List for isPrefixOf, sort, and group (isPrefixOf is used so much in this)
 -- - Data.List.Split to deal with the JSON files
 -- - Data.Ord for comparing, and more interesting sorting
 -- - System.Directory so we can muck about with files and directories
@@ -27,18 +27,18 @@ type Adj = ([Actor], Int)
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- A few test variables now:
 -- - limit is how far it should go before giving up on finding a link
--- - showsPath is where the shows are in my copy of the history-project repo
+-- - searchJSON is the location of "search.json", the file I use to make all this work
 -- - excludedShows is shows that are not taken into account
--- - And myself and some people as test cases for the actual degree-finder
+-- - nl is a newLine character as a string
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 limit :: Int
 limit = 1000
 searchJSON :: FilePath
 searchJSON = "search.json"
 excludedShows :: [ShowName]
-excludedShows = ["Charity Gala (2015-16)"] ++ ffGen
-ffGen :: [ShowName]
-ffGen = ["Freshers' Fringe (" ++ show (n-1) ++ "-" ++ drop 2 (show n) ++ ")" | n <- [2010..2030]]
+excludedShows = ["Charity Gala (2015-16)"] ++ ["Freshers' Fringe (" ++ show (n-1) ++ "-" ++ drop 2 (show n) ++ ")" | n <- [2010..2017]]
+nl :: String
+nl = "\n"
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Helpers!
@@ -49,40 +49,27 @@ rmDups :: (Eq a, Ord a) => [a] -> [a]       -- Removing duplicate entries in a s
 rmDups = map head . group . sort
 allActors :: [Details] -> [Actor]
 allActors = rmDups . flatten . map snd
-myReadFile :: FilePath -> IO String
-myReadFile = fmap T.unpack . TIO.readFile
 stripShit :: String -> String   -- Stripping out any characters that might surround an actor or show's name
 stripShit [] = []
 stripShit s                     -- Whitespace, quotation marks, colons, etc.
-  | hs == ' ' || hs == '\"' || hs == '[' || hs == ':' = stripShit (tail s)
-  | ls == ' ' || ls == '\"' || ls == ']' || ls == ',' = stripShit (init s)
-  | otherwise                                         = s
-  where hs = head s
-        ls = last s
+  | elem (head s) [' ','\"','[',':']  = stripShit (tail s)
+  | elem (last s) [' ','\"',']',',']  = stripShit (init s)
+  | otherwise                         = s
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- What we can do instead of all that is generate the list of Details from a JSON file, like so:
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 sJSONShows :: IO [[String]]   -- Reads the JSON file containing all the show information, returning it in a [[String]] format for processing
-sJSONShows = myReadFile searchJSON >>= return 
-                                       . filter (elem "        \"type\": \"show\",")  -- only actual shows allowed
-                                       . map (lines)                                  -- split on '\n' chars
-                                       . splitOn "\n    \n    \n\n    \n    ,"        -- having split the document on that horrible lot
+sJSONShows = (fmap T.unpack . TIO.readFile) searchJSON >>= return . filter (elem "        \"type\": \"show\",") . map lines . splitOn "\n    \n    \n\n    \n    ,"
 
 sJSONTitle :: [String] -> ShowName  -- Extracts the show title from sJSONShows
-sJSONTitle = stripShit . dropWhile (/=':') . head . filter (isInfixOf "\"title\":")
+sJSONTitle = stripShit . dropWhile (/=':') . head . filter (isPrefixOf "        \"title\": \"")
 
 sJSONYear :: [String] -> String -- Extracts the year of production; useful for clarity
-sJSONYear = flatten
-            . intersperse "-" 
-            . splitOn "&ndash;" 
-            . stripShit 
-            . dropWhile (/=':') 
-            . head 
-            . filter (isPrefixOf "        \"year_title\": ")
+sJSONYear = flatten . intersperse "-" . splitOn "&ndash;" . stripShit . dropWhile (/=':') . head . filter (isPrefixOf "        \"year_title\": \"")
 
 sJSONCast :: [String] -> [Actor]  -- Extracts the cast of the show
-sJSONCast = filter (/="") . map stripShit . splitOn ", " . stripShit . dropWhile (/=':') . head . filter (isInfixOf "\"cast\":")
+sJSONCast = filter (/="") . map stripShit . splitOn ", " . stripShit . dropWhile (/=':') . head . filter (isPrefixOf "        \"cast\": \"")
 
 notExcluded :: Details -> Bool  -- Rather than use a lambda function in filter, I'd rather just use an actual function, again for clarity
 notExcluded (s, _) = not (elem s excludedShows)
@@ -96,28 +83,29 @@ allDetails = sJSONShows >>= return . filter (\d -> notExcluded d && length (snd 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Finally, using everything above here, we can get two Actors, and return a printed String with the shortest link between them.
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
-baseAdj :: Actor -> [Adj] -- First, a brief function to turn an Actor into the most basic possible Adj WRT that Actor, i.e. themself and a degree of 0
-baseAdj a = [([a], 0)]
-
-allFellows :: Actor -> [Details] -> [Actor] -- Then a helper, a function to generate a list of every Actor one specific Actor has ever worked with.
+allFellows :: Actor -> [Details] -> [Actor] -- A helper, a function to generate a list of every Actor one specific Actor has ever worked with.
 allFellows a = filter (/=a) . rmDups . flatten . filter (elem a) . map snd
 
 fellowAdj' :: [Adj] -> [Details] -> [Actor] -> [Adj]  -- fellowAdj' takes a list of Adjs, and a list of explored Actors, and goes through each Adj generating
 fellowAdj' [] _ _             = []                  -- a list of new Adjs based on the head of the Actor list of each one
-fellowAdj' ((a, i):as) d done = let newFellows = [n | n <- allFellows (head a) d, not (elem n done || elem n a)] in [(new:a, i+1) | new <- newFellows] ++ fellowAdj' as d (newFellows ++ done)
+fellowAdj' ((a, i):as) d done = let newFellows = [n | n <- allFellows (head a) d, not (elem n done || elem n a)]
+                                in [(new:a, i+1) | new <- newFellows] ++ fellowAdj' as d (newFellows ++ done)
 
 fellowAdj :: [Adj] -> [Details] -> [Actor] -> [Adj] -- fellowAdj is then a recursive function that takes the list generated by fellowAdj'
 fellowAdj [] _ _    = []                            -- and reapplies fellowAdj' to that list, appending it to the first list
-fellowAdj as d done = let newList = fellowAdj' as d done in newList ++ fellowAdj newList d (map (head . fst) newList ++ done)
+fellowAdj as d done = let newList = fellowAdj' as d done
+                      in newList ++ fellowAdj newList d (map (head . fst) newList ++ done)
 
-allAdj :: Actor -> [Details] -> [Adj]   -- And allAdj takes fellowAdj and wraps it all up neatly
-allAdj a d = let baa = baseAdj a in baa ++ fellowAdj baa d []  -- so all you've to do is supply an Actor and a Detail list
+allAdj :: Actor -> [Details] -> [Adj]       -- And allAdj takes fellowAdj and wraps it all up neatly
+allAdj a d = let baa = [([a], 0)]           -- turn the Actor into the most basic Adj first
+             in baa ++ fellowAdj baa d []   -- then recursively generate the list of all Adjs
 
-adjLim :: Int -> Actor -> [Details] -> [Adj]  --allAdj is in theory an infinite list, so we use adjLim to limit it
-adjLim l a d = takeWhile ((<=l) . snd) (allAdj a d)
+adjLim :: Actor -> Int -> [Details] -> [Adj]  --allAdj is in theory an infinite list, so we use adjLim to limit it
+adjLim a l d = takeWhile ((<=l) . snd) (allAdj a d)
 
-adjSearch :: Actor -> Actor -> [Details] -> Adj -- adjSearch now takes the list generated by adjLim and if no list starts with the searched Actor, returns the two Actors and 1000 as an error code
-adjSearch a1 a2 d = let alList = filter ((== a1) . head . fst) (adjLim limit a2 d) in if alList == [] then ([a1,a2], 1000) else head alList
+adjSearch :: Actor -> Actor -> [Details] -> Adj
+adjSearch a1 a2 d = let alList = filter ((== a1) . head . fst) (adjLim a2 limit d)  -- Filters the list of all Adjs from a2 to just those with a1 at the head
+                    in if alList == [] then ([a1,a2], 1000) else head alList        -- If that list is empty: error code, otherwise return the head
 
 adjCheck :: Actor -> Actor -> [Details] -> Adj            -- adjCheck is basically input validation; it makes sure both Actors actually have records.
 adjCheck a1 a2 d                                          -- If they don't it returns an error code in the result, otherwise it runs adjSearch.
@@ -132,17 +120,18 @@ link a1 a2 d = "- " ++ a1 ++ " was in " ++ (fst . head . filter ((\as -> elem a1
 
 links :: [Actor] -> [Details] -> String -- links then applies this across a list of Actors, doing them two at a time
 links (a1:a2:[]) d = link a1 a2 d
-links (a1:a2:as) d = link a1 a2 d ++ "\n" ++ links (a2:as) d
+links (a1:a2:as) d = link a1 a2 d ++ nl ++ links (a2:as) d
 
 ppAdjCheck :: Actor -> Actor -> [Details] -> String -- Finally for the non-IO portion of this bit, ppAdjCheck takes the Actor names and the Detail list,
 ppAdjCheck a1 a2 d                                  -- performs adjCheck on them, and returns the appropriate String
-  | i == -3   = head as ++ " and " ++ last as ++ " are not Actors with records."
+  | i == -3   = hl ++ " are not Actors with records."
   | i == -2   = last as ++ " is not an Actor with a record."
   | i == -1   = head as ++ " is not an Actor with a record."
   | i == 0    = head as ++ " has 0 degrees of separation with themself by definition."
-  | i == 1000 = head as ++ " and " ++ last as ++ " are either not linked, or have more than " ++ show limit ++ " degrees of separation."
-  | otherwise = head as ++ " and " ++ last as ++ " have " ++ show i ++ " degrees of separation, and are linked as follows:\n" ++ links as d
+  | i == 1000 = hl ++ " are either not linked, or have more than " ++ show limit ++ " degrees of separation."
+  | otherwise = hl ++ " have " ++ show i ++ " degrees of separation, and are linked as follows:" ++ nl ++ links as d
   where (as, i) = adjCheck a1 a2 d
+        hl = head as ++ " and " ++ last as
 
 main' :: Actor -> Actor -> IO ()                          -- main' is where the IO starts; it feeds showDetails into ppAdjCheck
 main' a1 a2 = allDetails >>= putStrLn . ppAdjCheck a1 a2  -- and putStrLn's the resultant String so we get nice '\n' newlines
@@ -151,3 +140,5 @@ main :: IO ()           --main takes two getLines and returns main' with them as
 main = do a1 <- getLine
           a2 <- getLine
           main' a1 a2
+
+showCount = allDetails >>= putStrLn . flatten . intersperse "\n" . show . (\s -> head s ++ last s) . allActors
